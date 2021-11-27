522472bcdda1d1c93fe3dabc92733dff9dfd4614
greyhexy <whatoget@gmail.com>
2021-11-27 22:15:49 +0800

    add comment

--- cpp/basic/parameter-pack.cc
+++ cpp/basic/parameter-pack.cc
@@ -25,13 +25,22 @@ T add(Args ...args)
     return (... + args);
 }
 
-
 template<typename T>
 T contor_pair(T arg)
 {
     return arg;
 }
 
+/* f = contor_pair
+ *
+ * f(1, 2)
+ * k = 1 + 2 = 3 -> ans = (3 * 4) + 1 = 13
+ * 
+ * f(1, 2, 3)
+ * k = 1 + f(2, 3)
+ * -> 1 + f(2, 3) = 1 + (5 * 6 + 2) = 33
+ * ans = 33 * 34 + 1 = 990 + 132 + 1 = 1123
+ */
 template<typename T, typename... Args>
 T contor_pair(T arg1, Args... args)
 {
@@ -42,5 +51,5 @@ T contor_pair(T arg1, Args... args)
 int main()
 {
     // std::cout << add<int>(1, 2, 3, 4);
-    std::cout << contor_pair(1000, 1000, 10);
+    std::cout << contor_pair(1, 2, 3);
 }
