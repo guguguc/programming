c912993e3042c7544530964c7f1b2cfb5f5fc5ad
greyhexy <whatoget@gmail.com>
2022-05-07 17:36:42 +0800

    add os thread demos

---
+++ os/thread/sum-0.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define N 100000000
+
+long sum = 0;
+
+void Tsum(void)
+{
+    for (int i = 0; i < N; ++i) sum++;
+}
+
+
+int main()
+{
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, &Tsum, NULL);
+    pthread_create(&t2, NULL, &Tsum, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    printf("sum : %ld", sum);
+}
---
+++ os/thread/sum-2.c
@@ -0,0 +1,24 @@
+#include "spinlock.h"
+#include <pthread.h>
+#include <stdio.h>
+
+#define N 100000000
+
+long sum = 0;
+
+void Tsum(void) {
+  for (int i = 0; i < N; ++i) {
+      spinlock();
+      sum += 1;
+      spinunlock();
+  }
+}
+
+int main() {
+  pthread_t t1, t2;
+  pthread_create(&t1, NULL, &Tsum, NULL);
+  pthread_create(&t2, NULL, &Tsum, NULL);
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  printf("sum : %ld", sum);
+}
