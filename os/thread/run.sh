f8f0474dad34315743c322c2504bd6567e757b89
greyhexy <whatoget@gmail.com>
2022-05-07 20:48:34 +0800

    add gcc -g option

--- os/thread/run.sh
+++ os/thread/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p bin
-gcc sum-0.c -o bin/sum-0.out
-gcc sum-0.c -O1 -o bin/sum-0-gccO1.out
-gcc sum-0.c -O2 -o bin/sum-0-gccO2.out
-gcc sum-1.c -o bin/sum-1.out
-gcc sum-2.c spinlock.c -o bin/sum-2.out
+gcc -g sum-0.c -o bin/sum-0.out
+gcc -g sum-0.c -O1 -o bin/sum-0-gccO1.out
+gcc -g sum-0.c -O2 -o bin/sum-0-gccO2.out
+gcc -g sum-1.c -o bin/sum-1.out
+gcc -g sum-2.c spinlock.c -o bin/sum-2.out
